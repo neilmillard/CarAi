58ae2e3f24ea897ce4bbfad5c34de1995fd57c0e
Neil Millard <neil.millard@gmail.com>
2018-07-22 23:40:03 +0100

    Add Camera Script and other assets

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+	public float smoothSpeed = 10f;
+	public Vector3 offset;
+
+	void LateUpdate ()
+	{
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		transform.position = smoothedPosition;
+
+		transform.LookAt(target);
+	}
+}
